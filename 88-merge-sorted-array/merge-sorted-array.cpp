4abd8084a902a8b2daff2863a1175257ead7a92d
Samprit Saha <137399202+SampritSaha@users.noreply.github.com>
2025-06-22 16:12:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (95.50%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=m, j=0;
+        for(;j<n; i++,j++ ){
+            nums1[i] = nums2[j];
+        }
+        sort(nums1.begin(), nums1.end());
+        return;
+        
+        
+        
+        
+        
+        
+        
+        // Using another Vector then copyint but gave memory overflow
+        
+        // vector <int> v;
+        // int i=0,j=0;
+        // while(i<m && j<n){
+        //     if(nums1[i]>nums2[j]){
+        //         v.push_back(nums2[j]);
+        //     }
+        //     else{
+        //         v.push_back(nums1[i]);
+        //     }
+        // }
+        // while(i<m){
+        //     v.push_back(nums1[i]);
+        //     i++;
+        // }
+        // while(j<n){
+        //     v.push_back(nums2[j]);
+        //     j++;
+        // }
+        // for(int i=0; i<m+n; i++){
+        //     nums1[i] = v[i];
+        // }
+        // return;
+    }
+}; 
