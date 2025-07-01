82de8e87d6e12126d370c6aad5192d2d954505c3
Samprit Saha <137399202+SampritSaha@users.noreply.github.com>
2025-07-01 10:15:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (18.10%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -0,0 +1,36 @@
+class MyStack {
+public:
+    deque<int> q;
+    MyStack() {       
+    }
+    
+    void push(int x) {
+        q.push_back(x);
+    }
+    
+    int pop() {
+        int b=q.back();
+        q.pop_back();
+        return b;
+    }
+    
+    int top() {
+        return q.back();
+    }
+    
+    bool empty() {
+        if(q.empty()){
+            return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
