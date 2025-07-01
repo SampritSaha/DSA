c9c4fc5d0fc54a4398dd06b3d6ce12aac222fa5b
Samprit Saha <137399202+SampritSaha@users.noreply.github.com>
2025-07-01 10:33:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (18.10%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,30 +1,67 @@
 class MyStack {
 public:
-    deque<int> q;
-    MyStack() {       
-    }
-    
+    queue<int> q1, q2;
+
+    MyStack() {}
+
     void push(int x) {
-        q.push_back(x);
-    }
-    
-    int pop() {
-        int b=q.back();
-        q.pop_back();
-        return b;
-    }
-    
-    int top() {
-        return q.back();
-    }
-    
-    bool empty() {
-        if(q.empty()){
-            return true;
+        // Step 1: Push x to q2
+        q2.push(x);
+
+        // Step 2: Push all elements of q1 into q2
+        while (!q1.empty()) {
+            q2.push(q1.front());
+            q1.pop();
         }
-        return false;
+
+        // Step 3: Swap q1 and q2
+        swap(q1, q2);
     }
+
+    int pop() {
+        // Top element is at the front of q1
+        int topElem = q1.front();
+        q1.pop();
+        return topElem;
+    }
+
+    int top() {
+        return q1.front();  // Front is the top of stack
+    }
+
+    bool empty() {
+        return q1.empty();
+    }
+
+
+    // deque<int> q;
+    // MyStack() {       
+    // }
+    
+    // void push(int x) {
+    //     q.push_back(x);
+    // }
+    
+    // int pop() {
+    //     int b=q.back();
+    //     q.pop_back();
+    //     return b;
+    // }
+    
+    // int top() {
+    //     return q.back();
+    // }
+    
+    // bool empty() {
+    //     if(q.empty()){
+    //         return true;
+    //     }
+    //     return false;
+    // }
 };
+
+
+
 
 /**
  * Your MyStack object will be instantiated and called as such:
