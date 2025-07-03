79e48f172fc38f27a57e39406c21d5aebc1f1ad5
Samprit Saha <137399202+SampritSaha@users.noreply.github.com>
2025-07-03 17:55:56 +0530

    Time: 2 ms (70.08%) | Memory: 14.8 MB (6.60%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,15 +1,37 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        //brutforce
-        vector<int> ans(nums.size(), 0);
-        for(int i=0; i<nums.size(); i++){
-            for(int j = 0; j<nums.size(); j++){
-                if(nums[i] > nums[j]){
-                    ans[i]++;
-                }
+        
+         int n = nums.size();
+        vector<int> copy = nums;
+        sort(copy.begin(), copy.end());
+
+        unordered_map<int, int> indexMap;
+
+        for (int i = 0; i < n; i++) {
+            // Only assign the first occurrence to get the count of smaller numbers
+            if (indexMap.find(copy[i]) == indexMap.end()) {
+                indexMap[copy[i]] = i;
             }
         }
+
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            ans.push_back(indexMap[nums[i]]);
+        }
+
         return ans;
+        
+        
+        //brutforce
+        // vector<int> ans(nums.size(), 0);
+        // for(int i=0; i<nums.size(); i++){
+        //     for(int j = 0; j<nums.size(); j++){
+        //         if(nums[i] > nums[j]){
+        //             ans[i]++;
+        //         }
+        //     }
+        // }
+        // return ans;
     }
 };
